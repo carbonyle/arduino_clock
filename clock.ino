1a107f6182147b39705a5e92d71df2ca31ce2b04
carbonyle elynobrac <carbonyle@users.noreply.github.com>
2015-10-26 22:06:23 +0100

    Update clock.ino

--- clock.ino
+++ clock.ino
@@ -8,6 +8,7 @@ Colored clock for the Adafruit RGB Character LCD Shield + SD-RTC shield
 #include "RTClib.h"
 #include <utility/Adafruit_MCP23017.h>
 #include <Adafruit_RGBLCDShield.h>
+
 #define RED 0x1
 #define YELLOW 0x3
 #define GREEN 0x2
@@ -15,6 +16,7 @@ Colored clock for the Adafruit RGB Character LCD Shield + SD-RTC shield
 #define BLUE 0x4
 #define VIOLET 0x5
 #define WHITE 0x7
+#define aref_voltage 3.3 
 
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 RTC_DS1307 rtc;
@@ -23,8 +25,10 @@ DateTime now;
 void setup()
 {
   lcd.begin(16,2);
+  lcd.setBacklight(YELLOW);
   Wire.begin();
   rtc.begin();
+  //Serial.begin(9600); 
 }
 
 void loop() 
@@ -32,8 +36,8 @@ void loop()
   now = rtc.now();
   updateTime();
   updateColor();
-//  updateTemp();
-//  updateLight();
+  //updateSense();
+  updateLight();
 }
 
 void updateTime()
@@ -46,21 +50,30 @@ void updateTime()
  int d = now.day();  
  int M = now.month();
  int y = now.year(); 
+ int D = now.dayOfWeek();
  
  // Clock
- lcd.setCursor(0,1);
- if (h<10) {lcd.print(0);} lcd.print(h);
+ lcd.setCursor(6,1);
+ if (h-1<10) {lcd.print(0);} lcd.print(h-1);
  lcd.print(":");
  if (m<10) {lcd.print(0);} lcd.print(m);
  lcd.print(":");
  if (s<10) {lcd.print(0);} lcd.print(s);
 
  // Calendar
- lcd.setCursor(0,0);
+ lcd.setCursor(2,0);
+ if (D=1)lcd.print("Mon");
+ else if (D=2)lcd.print("Thu");
+ else if (D=3)lcd.print("Wed");
+ else if (D=4)lcd.print("Thu");
+ else if (D=5)lcd.print("Fri");
+ else if (D=6)lcd.print("Sat");
+ else if (D=7)lcd.print("Sun");
+ lcd.setCursor(6,0);
  if (d<10) {lcd.print(0);} lcd.print(d);
- lcd.print("/");
- if (M<10) {lcd.print(0);} lcd.print(M);
- lcd.print("/");
+ lcd.print(".");
+lcd.print(M);
+ lcd.print(".");
  lcd.print(y-2000);
 
 }
@@ -93,26 +106,20 @@ void updateColor()
 //void updateTemp() 
 //{
 
-//  int tempPin = 1; 
-//  int tempReading;
+  //int tempPin = 1; 
+  //int tempReading;
 
-//  tempReading = analogRead(tempPin);  
-//  lcd.setCursor(9,0);
-//  lcd.print(tempReading);
 
 //}
 
-//void updateLight() 
-//{
+void updateLight() 
+{
 
-//  int photocellPin = 0; 
-//  int photocellReading; 
+  int photocellPin = 0; 
+  int photocellReading; 
 
-//  photocellReading = analogRead(photocellPin);
-//  lcd.setCursor(9,1); 
-//  lcd.print(photocellReading);
+  photocellReading = analogRead(photocellPin);
+  lcd.setCursor(2,1); 
+  lcd.print(photocellReading);
 
-//}
-
-
-
+}
